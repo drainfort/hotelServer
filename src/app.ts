cf423b2d25215083524b4903ea949f5b518355ae
Jaime <jaime.romero @skaphe.com>
2018-08-17 20:25:00 -0500

    Enable cors on server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import * as express from 'express'
-import apiRouter from './api/routes/api.route'
+import * as cors from "cors";
+import apiRouter from './api/routes/api.route';
+import {NextFunction, Request, Response} from "express";
 
 class App {
   public app
 
   constructor () {
     this.app = express();
+    this.app.use(cors());
+    this.app.use(function (req: Request, res: Response, next: NextFunction) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token");
+        next();
+    });
     this.mountRoutes();
   }
 
